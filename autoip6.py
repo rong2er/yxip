b88f236099c5839c93cce9e9acb9b081fd5968e9
rong2er <hexianfu520@gmail.com>
2025-11-12 22:03:19 +0800

    Update autoip6.py

--- autoip6.py
+++ autoip6.py
@@ -21,8 +21,7 @@ urls = [
     'https://www.wetest.vip/page/cloudflare/address_v6.html',
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
     'https://cf.090227.xyz',
-    'https://stock.hostmonit.com/CloudFlareYes'，
-    'https://raw.githubusercontent.com/rong2er/CFIP/refs/heads/main/ip.txt'
+    'https://stock.hostmonit.com/CloudFlareYes'
 ]
 
 # 正则表达式用于初步匹配IPV4与IPV6地址(配合ipaddress库二次过滤)
