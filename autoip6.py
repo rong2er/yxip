066220a6a1a03f703921c249e9eea109692881a7
rong2er <hexianfu520@gmail.com>
2025-11-13 15:13:59 +0800

    Update autoip6.py

--- autoip6.py
+++ autoip6.py
@@ -22,7 +22,6 @@ urls = [
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
     'https://cf.090227.xyz',
     'https://stock.hostmonit.com/CloudFlareYes',
-    'https://raw.githubusercontent.com/rong2er/CFIP/refs/heads/main/ip.txt',
     'https://raw.githubusercontent.com/rong2er/Senflare-IP/refs/heads/main/Ranking.txt'
 ]
 
